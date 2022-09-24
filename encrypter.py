664c0520af3c35d64e26a09cb32ac572f842045a
Andrey <62668026+AndreyRojasD@users.noreply.github.com>
2022-09-24 11:19:29 -0600

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -7,7 +7,7 @@ print("Introduce para que plataforma es tu contraseña")
 plataforma = input()
 print("Introduce clave de encriptacion para tu archivo de contraseñas ")
 passw = getpass.getpass("Contraseña: ")
-nis = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9''@','#','$','%','^','&','*','(',')','-']
+nis = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','W','E','R','T','Y','U','I','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9''@','#','$','%','^','&','*','(',')','-']
 num = 1
 ctep1 = random.choice(nis)
 ctep2 = random.choice(nis)
